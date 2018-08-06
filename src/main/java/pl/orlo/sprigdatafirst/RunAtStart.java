45dc633397aa00d9519b1cd462c4d5a5d1a34c43
Marek Orłowski <morlowski@ittouch.pl>
2018-08-06 08:51:12 +0200

    commit

--- src/main/java/pl/orlo/sprigdatafirst/RunAtStart.java
+++ src/main/java/pl/orlo/sprigdatafirst/RunAtStart.java
@@ -3,6 +3,8 @@ package pl.orlo.sprigdatafirst;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import pl.orlo.sprigdatafirst.entitys.Employee;
@@ -40,6 +42,12 @@ public class RunAtStart {
 
         logger.info("SORTED BY FIRST NAME AND LAST NAME");
         printAll(getSortedByFirstNameAndLastName());
+
+        Page<Employee> page = employeeRepository.findAll(new PageRequest(2, 10));
+        logger.info("TOTAL NUMBER OF EMPLOYEES" + page.getTotalElements());
+        logger.info("TOTAL NUMBER OF PAGES" + page.getTotalPages());
+        logger.info("ELEMENTS ON PAGE: ");
+        printAll(page.getContent());
     }
 
     private List<Employee> getSortedByFirstNameAndLastName() {
