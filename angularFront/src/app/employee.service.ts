0ead79174198eb9ca6914a2d0e0c09971f87f253
Marek Orłowski <morlowski@ittouch.pl>
2018-08-10 14:21:41 +0200

    obsługa błędu

--- angularFront/src/app/employee.service.ts
+++ angularFront/src/app/employee.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
 import {Http} from "@angular/http";
 import {Employee} from "./dto/dto";
-import { Observable, Subject } from 'rxjs';
+import {Observable, Subject, throwError} from 'rxjs';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import {HttpErrorResponse} from "@angular/common/http";
 @Injectable()
 export class EmployeeService {
 
@@ -17,7 +18,7 @@ export class EmployeeService {
   public getEmployees(): Observable<Employee[]> {
     return this.http.get('api/empl')
       .map(resp=>resp.json())
-      .catch(this.handleErrorObservable);
+      .catch(this.handleError);
   }
 
   private handleErrorObservable(error: Response | any) {
@@ -25,4 +26,19 @@ export class EmployeeService {
     return Observable.throw(error.message || error);
   }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.log('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.log(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 }
